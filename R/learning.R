1b3d03c2a22a9a413943563272f80a146c6f0f58
Menghan Gao <amiemengmeng@outlook.com>
2023-01-16 16:02:09 +0100

    Added comment

---
+++ R/learning.R
@@ -0,0 +1,31 @@
+10
+
+# R basics ----------------------------------------------------------------
+# Command shit P : search “section” to add a new coding section
+weight_kilos <- 100
+weight_kilos <- 10
+weight_kilos
+
+# C() is an option
+
+weight_kilos
+
+colnames(airquality)
+
+str(airquality)
+# str means structure, it's to check the data set
+
+summary(airquality)
+
+2 + 2
+
+2 + 2
+
+# Two ways to restyle the code 1. highlight, then Code > Reform Code 2. Crl+shift+P, then search "Style active file"
+
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+
+# This will be used for testing out Git.
